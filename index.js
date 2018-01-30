c69e17c212eec60dc6d502cc473ba9990128b6b1
Michael Likouris <mlikouris@perfectsensedigital.com>
2018-01-29 21:53:04 -0500

    First commit for albums project"

---
+++ index.js
@@ -0,0 +1,12 @@
+// Import a library to help create a compontent
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
+
+// Create a compontent
+const App = () => (
+    <Header headerText='Albums' />
+  );
+
+// Render it to the device
+AppRegistry.registerComponent('albums', () => App);
