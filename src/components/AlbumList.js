d19442d6bbbd78d3c35c32d59dc3a2e2c88c1d0c
Michael Likouris <mlikouris@perfectsensedigital.com>
2018-02-01 22:58:32 -0500

    returning data for the album list

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,11 +1,30 @@
 import React, {Component} from 'react'
-import {View, Text} from 'react-native'
+import {View} from 'react-native'
+import AlbumDetail from './AlbumDetail'
+import axios from 'axios'
 
 class AlbumList extends Component {
-  render () {
+  state = { albums: [] }
+
+  componentWillMount() {
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+    .then(response => this.setState({albums: response.data }))
+  }
+
+  componentWillUnmount() {
+
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album =>
+      <AlbumDetail key={album.title} album={album} />
+    )
+  }
+
+  render() {
     return (
       <View>
-      <Text>Album List!!!</Text>
+      {this.renderAlbums()}
       </View>
     )
   }
