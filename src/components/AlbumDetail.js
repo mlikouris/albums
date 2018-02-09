8f6b24c99a22bfaeccddc7b0c0baf7bdc1658150
Michael Likouris <mlikouris@perfectsensedigital.com>
2018-02-08 22:55:13 -0500

    Adding alum detail and list

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,13 +1,69 @@
 import React from 'react'
-import {Text} from 'react-native'
+import {Image, Text, View, Linking} from 'react-native'
 import Card from './Card'
+import CardSection from './CardSection'
+import Button from './Button'
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({album}) => {
+  const {artist, title, thumbnail_image, image, url} = album
+  const {
+    thumbnailStyle,
+    headerContentStyle,
+    thumbnailContainerStyle,
+    headerTextStyle,
+    imageStyle
+   } = styles
+
   return (
     <Card>
-      <Text>{props.album.title}</Text>
+      <CardSection>
+        <View style={thumbnailContainerStyle}>
+          <Image
+            style={thumbnailStyle}
+            source={{ uri: thumbnail_image }}
+          />
+        </View>
+        <View syle={headerContentStyle}>
+          <Text style={headerTextStyle}>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
+      <CardSection>
+        <Image
+          style={imageStyle}
+          source={{ uri: image }}
+        />
+      </CardSection>
+      <CardSection>
+        <Button onPress={() => Linking.openURL(url)}>
+          Buy Now
+        </Button>
+      </CardSection>
     </Card>
   )
 }
-
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  headerTextStyle: {
+    fontSize: 18
+  },
+  thumbnailStyle: {
+    width: 50,
+    height: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 10,
+    marginLeft: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
+  }
+}
 export default AlbumDetail
